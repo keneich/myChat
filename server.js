e46841991f1a35158a1b74f458cb47781ccaf44b
keneich <keneich2@gmail.com>
2026-04-08 00:43:49 +0900

    6st app

--- server.js
+++ server.js
@@ -64,7 +64,14 @@ wss.on('connection', (ws) => {
         type: 'chat',
         nickname,
         text,
-        time: new Date().toLocaleTimeString('ko-KR', { hour: '2-digit', minute: '2-digit' }),
+        time: new Date().toLocaleString('ko-KR', {
+          timeZone: 'Asia/Seoul',
+          year: 'numeric',
+          month: '2-digit',
+          day: '2-digit',
+          hour: '2-digit',
+          minute: '2-digit',
+        }),
       });
     }
   });
